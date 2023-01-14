12a0543bf8b2eeb140d1a57867bdfc84de248373
seichi <seichi0421@outlook.jp>
2023-01-14 19:30:59 +0900

    Update src/models/components/posterior_encoder.py
    Co-authored-by: GesonAnko <59220704+Geson-anko@users.noreply.github.com>

--- src/models/components/posterior_encoder.py
+++ src/models/components/posterior_encoder.py
@@ -4,9 +4,9 @@ from typing import Optional, Tuple
 
 import torch
 
-from src.models.components.wavenet import WaveNet
-from src.models.components.wavenet.residual_block import Conv1d
-from src.utils.nets_utils import make_non_pad_mask
+from .wavenet import WaveNet
+from .wavenet.residual_block import Conv1d
+from ...utils.nets_utils import make_non_pad_mask
 
 
 class PosteriorEncoder(torch.nn.Module):
