a5fc1ccd6c625f3ae4e39a5e524b3e449ad61af5
cehl-kurage <yusakuishihira@gmail.com>
2023-01-19 01:06:42 +0900

    ADD test__init__ and test_init_values

--- tests/utils/test_replay_buffer.py
+++ tests/utils/test_replay_buffer.py
@@ -1,10 +1,23 @@
+import pytest
 import numpy as np
 
 from src.utils.replay_buffer import ReplayBuffer
+from gym.spaces import Box
+
+spaces = {
+    "space1": Box(-1, 1, (3,)),
+    "space2": Box(-np.inf, 1, (3,)),
+    "space3": Box(-1, np.inf, (3,)),
+    "space4": Box(-np.inf, np.inf, (3,)),
+}
+valid_buffer_size = 30000
+invalid_buffer_size = -1
 
 def test__init__():
-    pass
+    with pytest.raises(ValueError) as e:
+        rb = ReplayBuffer(spaces, invalid_buffer_size)
 
+    
 def test_push():
     pass
 
@@ -18,4 +31,7 @@ def test__len__():
     pass
 
 def test_init_values():
-    pass
+    rb = ReplayBuffer(spaces, valid_buffer_size)
+    for space_name, box in spaces.items():
+        assert rb.memory.get(space_name) is not None
+        assert len(rb.memory.get(space_name)) == box.shape[0]
