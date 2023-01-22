2352234b41e0b8dd5158b495e4e47b0fb43fe88b
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-01-22 04:37:38 +0000

    ADD empty train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,45 @@
 # このファイルは直接実行するため、相対import文を記述しないでください。
+import logging
+from typing import Optional
+
+import hydra
+import pyrootutils
+from omegaconf import DictConfig
+
+pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+
+# Copied from https://github.com/ashleve/lightning-hydra-template/blob/main/src/train.py
+# ------------------------------------------------------------------------------------ #
+# the setup_root above is equivalent to:
+# - adding project root dir to PYTHONPATH
+#       (so you don't need to force user to install project as a package)
+#       (necessary before importing any local modules e.g. `from src import utils`)
+# - setting up PROJECT_ROOT environment variable
+#       (which is used as a base for paths in "configs/paths/default.yaml")
+#       (this way all filepaths are the same no matter where you run the code)
+# - loading environment variables from ".env" in root dir
+#
+# you can remove it if you:
+# 1. either install project as a package or move entry files to project root dir
+# 2. set `root_dir` to "." in "configs/paths/default.yaml"
+#
+# more info: https://github.com/ashleve/pyrootutils
+# ------------------------------------------------------------------------------------ #
+
+logger = logging.getLogger(__name__)
+
+
+def train(cfg: DictConfig) -> None:
+    """Trains the model."""
+    logger.info("Training started!")
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="train.yaml")
+def main(cfg: DictConfig) -> Optional[float]:
+
+    # train the model
+    train(cfg)
+
+
+if __name__ == "__main__":
+    main()
