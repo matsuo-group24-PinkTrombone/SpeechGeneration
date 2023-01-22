10ea69977326046d2b745c32d5bfc270512c00f3
cehl-kurage <yusakuishihira@gmail.com>
2023-01-22 23:31:19 +0900

    FIX test_forward regarding batch dimention

--- tests/models/components/test_prior_encoder.py
+++ tests/models/components/test_prior_encoder.py
@@ -7,7 +7,7 @@ hidden_dim = 10
 invalid_hidden_dim = -1
 state_dim = 5
 invalid_state_dim = -1
-
+batch_size = 4
 
 def test__init__():
     with pytest.raises(AssertionError):
@@ -18,9 +18,15 @@ def test__init__():
 
 def test_forward():
     model = Prior(hidden_dim, state_dim)
+    # single input
     random_input = torch.rand((hidden_dim,), requires_grad=True)
     state_distribution = model(random_input)
     assert state_distribution.sample().shape == (state_dim,)
+    assert state_distribution.rsample().requires_grad == True
+    # batch input
+    random_input = torch.rand((batch_size, hidden_dim), requires_grad=True)
+    state_distribution = model(random_input)
+    assert state_distribution.sample().shape == (batch_size, state_dim)
     assert state_distribution.rsample().requires_grad == True
 
 
