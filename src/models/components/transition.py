1dddda85b363d682e6a9d110517bace092f685c7
cehl-kurage <yusakuishihira@gmail.com>
2023-01-23 22:54:25 +0900

    ADD some attributes in __init__

--- src/models/components/transition.py
+++ src/models/components/transition.py
@@ -20,15 +20,20 @@ class Transition(AbstractTransition):
             state_size (int): The size of state(s_t)
             action_size (int): The size of action(a_t)
             input_size (int): The size of vector input to RNN
-            bias (bool, optional): This argment determines whether RNN requires bias or not. Defaults to True.
+            bias (bool, optional): This argument determines whether RNN requires bias or not. Defaults to True.
         """
         super().__init__()
+        self.hidden_size = hidden_size
+        self.state_size = state_size
+        self.action_size = action_size
+        self.input_size = input_size
+        self.bias = bias
         self.rnn = nn.GRUCell(
-            input_size=input_size,
-            hidden_size=hidden_size,
+            input_size=self.input_size,
+            hidden_size=self.hidden_size,
             bias=bias,
         )
-        self.fc_action_state = nn.Linear(state_size + action_size, input_size)
+        self.fc_action_state = nn.Linear(self.state_size + self.action_size, self.input_size)
 
     def forward(self, hidden: _toa, state: _toa, action: _toa) -> torch.Tensor:
         """Method for computing f(h_t, s_t, a_t) -> h_t+1.
