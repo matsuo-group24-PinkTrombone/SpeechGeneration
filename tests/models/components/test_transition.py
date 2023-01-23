464e327aa72da7b7067dc0fe98a2ea5f78987865
cehl-kurage <yusakuishihira@gmail.com>
2023-01-23 22:52:02 +0900

    ADD test__init__, test__call__, and test_hidden_shape

--- tests/models/components/test_transition.py
+++ tests/models/components/test_transition.py
@@ -7,10 +7,25 @@ action_size = 8
 state_size = 8
 input_size = 8
 batch_size = 4
+args = {
+    "hidden_size": hidden_size,
+    "action_size": action_size,
+    "state_size": state_size,
+    "input_size": input_size,
+}
+
+
+def test__init__():
+    model = Transition(**args)
+
+    assert model.hidden_size == hidden_size
+    assert model.action_size == action_size
+    assert model.state_size == state_size
+    assert model.input_size == input_size
 
 
 def test_forward():
-    model = Transition(hidden_size, state_size, action_size, input_size)
+    model = Transition(**args)
 
     # single input
     random_state = torch.rand(state_size)
@@ -26,3 +41,12 @@ def test_forward():
     next_hidden = model(random_hidden, random_state, random_action)
     assert next_hidden.size(-1) == hidden_size
     assert next_hidden.size(0) == batch_size
+
+
+def test_hidden_shape():
+    model = Transition(**args)
+    assert model.hidden_shape == (hidden_size,)
+
+
+def test__call__():
+    test_forward()
