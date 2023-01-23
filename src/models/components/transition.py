ec4e4887e6e4f4e2fa7bb0f5b65416ada94fb2b3
cehl-kurage <yusakuishihira@gmail.com>
2023-01-23 11:47:22 +0900

    Fix class name

--- src/models/components/transition.py
+++ src/models/components/transition.py
@@ -1,9 +1,11 @@
 import torch
 from torch import nn
-from src.models.abc.transition import Transition
-from src.models.abc._types import _tensor_or_any as _toa
 
-class TransitionModel(Transition):
+from src.models.abc._types import _tensor_or_any as _toa
+from src.models.abc.transition import Transition as AbstractTransition
+
+
+class Transition(AbstractTransition):
     def __init__(
         self,
         hidden_size: int,
@@ -19,8 +21,8 @@ class TransitionModel(Transition):
             bias=bias,
         )
         self.fc_action_state = nn.Linear(state_size + action_size, input_size)
-        
-    def forward(self, hidden:_toa, state:_toa, action:_toa)->torch.Tensor:
+
+    def forward(self, hidden: _toa, state: _toa, action: _toa) -> torch.Tensor:
         rnn_input = self.fc_action_state(torch.cat((state, action), dim=-1))
         print(rnn_input.shape)
         print(hidden.shape)
@@ -31,5 +33,5 @@ class TransitionModel(Transition):
     def hidden_shape(self) -> tuple[int]:
         return (self.hidden_size,)
 
-    def __call__(self, hidden:_toa, state:_toa, action:_toa)->torch.Tensor:
+    def __call__(self, hidden: _toa, state: _toa, action: _toa) -> torch.Tensor:
         return self.forward(hidden, state, action)
