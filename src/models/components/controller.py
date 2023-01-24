82929310b677edd13d919737b97fc4741c95c83a
seichi <seichi8901@gmail.com>
2023-01-24 13:32:35 +0000

    メルスペクトログラムからエンコードするように修正

--- src/models/components/controller.py
+++ src/models/components/controller.py
@@ -1,15 +1,19 @@
 import torch
 from torch import Tensor
+from typing import Tuple
 
 from ..abc.controller import Controller as AbsController
-
+from ...utils.nets_utils import make_non_pad_mask
+from .wavenet import WaveNet
+from .wavenet.residual_block import Conv1d
 
 class Controller(AbsController):
     def __init__(
         self,
         hidden_dim: int,
         state_dim: int,
-        target_dim: int,
+        encoder_modules: Tuple,
+        feats_T: int,
         c_hidden_dim: int,
         action_dim: int,
         bias: bool,
@@ -18,22 +22,27 @@ class Controller(AbsController):
         Args:
             hidden_dim (int):
             state_dim (int):
-            target_dim (int):
             c_hidden_dim (int):
         """
+
+        # define target melspectrogram encoder
+        # define modules
+
         super().__init__()
         self.hidden_dim = hidden_dim
         self.state_dim = state_dim
-        self.target_dim = target_dim
+        self.input_conv,self.encoder,self.encoder_proj = encoder_modules
         self.c_hidden_dim = c_hidden_dim
+        self.action_dim = action_dim
 
+        self.state_emb = torch.nn.Linear(feats_T,1)
         self.rnn = torch.nn.GRUCell(
-            input_size=hidden_dim + state_dim + target_dim,
+            input_size=hidden_dim + state_dim * 2,
             hidden_size=c_hidden_dim,
             bias=bias,
         )
         self.proj = torch.nn.Sequential(
-            torch.nn.Linear(c_hidden_dim, action_dim * 2), torch.nn.Tanh()
+            torch.nn.Linear(c_hidden_dim, action_dim), torch.nn.Tanh()
         )
 
     def forward(
@@ -48,23 +57,36 @@ class Controller(AbsController):
         Args:
             hidden (Tensor):[B,hidden_dim]
             state (Tensor):[B,state_dim]
-            target (Tensor):[B,target_dim]
+            target (Tensor):melspectrogram [B,in_channels, T_feats]
             cotroller_hidden (Tensor):[B,c_hidden_dim]
             probabilistic (bool):  If True, sample action from normal distribution.
         """
+        # target encoding
+        x = self.input_conv(target)
+        x = self.encoder(x, None,g=None)
+        enc_stats = self.encoder_proj(x)
+        enc_m, enc_logs = enc_stats.split(enc_stats.size(1) // 2, dim=1)
+        target_state = (enc_m + torch.randn_like(enc_m) * torch.exp(enc_logs))
+        
+        t_state_emb = self.state_emb(target_state).squeeze(2)
+
         # RNN
-        hidden_state = torch.cat((hidden, state), dim=1)
-        hidden_state_target = torch.cat((hidden_state, target), dim=1)
+        state_emb = self.state_emb(state).squeeze(2)
+        hidden_state = torch.cat((hidden, state_emb), dim=1)
+        hidden_state_target = torch.cat((hidden_state, t_state_emb), dim=1)
 
         next_controller_hidden = self.rnn(hidden_state_target, controller_hidden)
 
         # action
-        stats = self.proj(next_controller_hidden)
-        m, logs = stats.split(stats.size(1) // 2, dim=1)
         if probabilistic:
-            action = m + torch.randn_like(m) * torch.exp(logs)
+            action = torch.clip(
+                torch.normal(mean=0,std=1,size=(hidden.size(0),self.action_dim)),
+                min = -1,
+                max = 1
+            )
         else:
-            action = m
+            assert next_controller_hidden.size() == torch.Size([controller_hidden.size(0),self.c_hidden_dim])
+            action = self.proj(next_controller_hidden)
 
         return action, next_controller_hidden
 
--- tests/models/components/test_controller.py
+++ tests/models/components/test_controller.py
@@ -2,37 +2,48 @@ import pytest
 import torch
 
 from src.models.components.controller import Controller
-
+from src.models.components.posterior_encoder_vits import PosteriorEncoderVITS
 
 @pytest.mark.parametrize(
     """
     batch_size,
     hidden_dim,
     state_dim,
-    target_dim,
+    target_mel_channels,
+    feats_T,
     c_hidden_dim,
     action_dim,
     prob
     """,
     [
-        (1, 192, 192, 192, 192, 7, False),
-        (5, 12, 12, 12, 12, 44, True),
+        (1, 192, 192, 80, 5, 192, 7, False),
+        (5, 12, 12, 80, 5, 12, 44, True),
     ],
 )
 def test_controller(
     batch_size: int,
     hidden_dim: int,
     state_dim: int,
-    target_dim: int,
+    target_mel_channels: int,
+    feats_T: int,
     c_hidden_dim: int,
     action_dim: int,
     prob: bool,
 ):
+    # define encoder_modules
+    pos_enc_vits = PosteriorEncoderVITS(
+        in_channels=target_mel_channels,
+        hidden_channels=state_dim,
+        out_channels=state_dim
+    )
+    encoder_modules = pos_enc_vits.get_encoder_modules()
+
     # controller instanse
     controller = Controller(
         hidden_dim=hidden_dim,
         state_dim=state_dim,
-        target_dim=target_dim,
+        encoder_modules=encoder_modules,
+        feats_T=feats_T,
         c_hidden_dim=c_hidden_dim,
         action_dim=action_dim,
         bias=True,
@@ -40,8 +51,9 @@ def test_controller(
 
     # create random tensor
     hidden = torch.randn(batch_size, hidden_dim)
-    state = torch.randn(batch_size, state_dim)
-    target = torch.randn(batch_size, target_dim)
+    state = torch.randn(batch_size, state_dim,feats_T)
+    target = torch.randn(batch_size, target_mel_channels,feats_T)
+    assert target.transpose(1,2).size() == torch.Size([batch_size,feats_T,target_mel_channels])
     c_hidden = torch.randn(batch_size, c_hidden_dim)
 
     action, next_controller_hidden = controller(hidden, state, target, c_hidden, prob)
