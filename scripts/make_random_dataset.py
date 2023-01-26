1f76977df81f4c8d78c16cda751144d91f52c786
cehl-kurage <yusakuishihira@gmail.com>
2023-01-26 18:51:12 +0900

    format

--- scripts/make_random_dataset.py
+++ scripts/make_random_dataset.py
@@ -1,31 +1,37 @@
 import glob
-import pathlib
 import math
 import os
-from typing import Callable
+import pathlib
 from random import uniform
+from typing import Callable
 
 import gym
 import numpy as np
 import soundfile
-
+import tqdm
 from pynktrombonegym.spaces import ObservationSpaceNames as OSN
 from pynktrombonegym.wrappers import ActionByAcceleration, Log1pMelSpectrogram
 
 target_sound_files = glob.glob("data/sample_target_sounds/*.wav")
-sound_seconds_range = (2., 3.)
-output_dir = pathlib.Path(__file__).parent.parent.joinpath("data/generated_randomly")
+sound_seconds_range = (2.0, 3.0)
+data_dir = pathlib.Path(__file__).parent.parent.joinpath("data")
+output_dir = "generated_randomly"
+sample_dir = "sample_generated_randomly"
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
+if not os.path.exists(sample_dir):
+    os.makedirs(sample_dir)
 
 
-def generate_sound(environment: gym.Env, action_fn: Callable, file_name: str, generate_chunk: int, sample_rate) -> None:
+def generate_sound(
+    environment: gym.Env, action_fn: Callable, file_name: str, generate_chunk: int, sample_rate
+) -> None:
     """Generate sound wave with environment and action_fn
     Args:
-        enviroment (env.PynkTrombone): Vocal tract environment.
+        environment (env.PynkTrombone): Vocal tract environment.
         action_fn (Callable): Return action for generating waves with environment.
-            This funcion must be able to receive `PynkTrombone` environment and
+            This function must be able to receive `PynkTrombone` environment and
             to return action.
             Ex:
             >>> def update_fn(environment: gym.Env):
@@ -34,7 +40,7 @@ def generate_sound(environment: gym.Env, action_fn: Callable, file_name: str, ge
     Returns:
         wave (np.ndarray): Generated wave. 1d array.
     """
-    sound_seconds = uniform(*sound_seconds_range)   
+    sound_seconds = uniform(*sound_seconds_range)
     roop_num = math.ceil(sound_seconds / (generate_chunk / sample_rate))
     generated_waves = []
     environment.reset()
@@ -50,7 +56,7 @@ def generate_sound(environment: gym.Env, action_fn: Callable, file_name: str, ge
 
     generated_sound_wave = np.concatenate(generated_waves).astype(np.float32)
 
-    path = os.path.join(output_dir, file_name)
+    path = os.path.join(data_dir, file_name)
     soundfile.write(path, generated_sound_wave, sample_rate)
 
 
@@ -62,7 +68,20 @@ if __name__ == "__main__":
     def action_fn(e: gym.Env) -> dict:
         return e.action_space.sample()
 
-    num_repeat = math.ceil(10 * 60 ** 2 / 2.5)
-    for i in range(num_repeat):
-        generate_sound(wrapped, action_fn, f"{i + 1}.wav", env.generate_chunk, env.sample_rate)
+    num_repeat = math.ceil(10 * 60**2 / 2.5)
+    # generate sounds for test
+    for i in range(5):
+        generate_sound(
+            wrapped,
+            action_fn,
+            f"{sample_dir}/{i}.wav",
+            env.generate_chunk,
+            env.sample_rate,
+        )
+
+    # generate sounds for dataset
+    for i in tqdm.tqdm(range(num_repeat)):
+        generate_sound(
+            wrapped, action_fn, f"{output_dir}/{i + 1}.wav", env.generate_chunk, env.sample_rate
+        )
     wrapped.close()  # you must call.
