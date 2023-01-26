a081bc023a7df463733451fadef4e809b7147db8
cehl-kurage <yusakuishihira@gmail.com>
2023-01-26 13:21:33 +0900

    ADD test file

---
+++ tests/scripts/test_make_random_dataset.py
@@ -0,0 +1,14 @@
+import glob
+import pytest
+from pydub import AudioSegment
+target_sample_files= "data/sample_generated_sounds/*.wav"
+
+test_files = glob.glob(target_sample_files)
+print(type(test_files[0]))
+@pytest.mark.parametrize("test_file", test_files)
+def test_make_random_dataset(test_file:str):
+    sound = AudioSegment.from_file(test_file)
+    assert 2.0 <= len(sound) / 1000 <= 3.0
+
+
+
