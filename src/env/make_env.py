96c401986e49bbf394afee816967e2225a2984f4
takeshun256 <takeshun1619@gmail.com>
2023-01-28 18:55:47 +0000

    solve wrapper error

--- src/env/make_env.py
+++ src/env/make_env.py
@@ -1,14 +1,14 @@
 import os
 
 import gym
-from array_action import ArrayAction
-from array_voc_state import ArrayVocState
-from normalize_action_range import NormalizeActionRange
 from omegaconf import DictConfig
-from pynktrombonegym.env import PynkTrombone
 from pynktrombonegym.wrappers.action_by_acceleration import \
     ActionByAcceleration
 from pynktrombonegym.wrappers.log1p_mel_spectrogram import Log1pMelSpectrogram
+
+from .array_action import ArrayAction
+from .array_voc_state import ArrayVocState
+from .normalize_action_range import NormalizeActionRange
 
 
 def make_env(configs: DictConfig) -> gym.Env:
@@ -16,7 +16,7 @@ def make_env(configs: DictConfig) -> gym.Env:
     Creates an environment instance from a list of audio dir paths.
     
     Args:
-    configs: Any: A dictionary of configurations, including 'dataset_dirs' and 'file_exts'.
+    configs: Any: A dictionary of configurations.
     
     Returns:
     gym.Env: The created environment instance.
@@ -29,12 +29,11 @@ def make_env(configs: DictConfig) -> gym.Env:
             files.extend(
                 [os.path.join(dataset_dir, f) for f in os.listdir(dataset_dir) if f.endswith(ext)]
             )
-    env = PynkTrombone(files)
 
     # ラッパーを順番に適用
-    env = Log1pMelSpectrogram(env)
-    env = ActionByAcceleration(env)
+    env = Log1pMelSpectrogram(files)
+    env = ActionByAcceleration(env, action_scaler=configs["action_scaler"])
     env = NormalizeActionRange(env)
-    env = ArrayAction(env)
-    env = ArrayVocState(env)
+    env = ArrayAction(env, new_step_api=configs["ArrayAction_new_step_api"])
+    env = ArrayVocState(env, new_step_api=configs["ArrayVocState_new_step_api"])
     return env
--- tests/env/test_make_env.py
+++ tests/env/test_make_env.py
@@ -6,13 +6,17 @@ from omegaconf import OmegaConf
 
 from src.env.make_env import make_env
 
+sample_target_sound_dir_paths = glob.glob("data/**/", recursive=False)
 
 def test_make_env():
-    dataset_dirs = "data/*"
-    dict_conf = {
-        "dataset_dirs": glob.glob(dataset_dirs),
-        "file_exts": [".wav"]
-    }
-    conf = OmegaConf.create(dict_conf)
-    env = make_env(conf)
+    configs = OmegaConf.create(
+        {
+            "dataset_dirs": sample_target_sound_dir_paths,
+            "file_exts": [".wav"],
+            "action_scaler": 1.0,
+            "ArrayAction_new_step_api": True,
+            "ArrayVocState_new_step_api": True,
+        }
+    )
+    env = make_env(configs)
     assert isinstance(env, gym.Env)
