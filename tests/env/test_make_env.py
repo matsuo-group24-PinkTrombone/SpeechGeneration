38ed8f93d6d04f99f619b824ed78d03dc0b126d6
takeshun256 <takeshun1619@gmail.com>
2023-01-28 21:54:00 +0000

    update test_make_env.py for testing create_file_list func

--- tests/env/test_make_env.py
+++ tests/env/test_make_env.py
@@ -1,20 +1,76 @@
 
 import glob
+import os
+import tempfile
 
 import gym
+import numpy as np
+from gym.spaces import Box
 from omegaconf import OmegaConf
 
-from src.env.make_env import make_env
+from src.env.make_env import apply_wrappers, create_file_list, make_env
 
-sample_target_sound_dir_paths = glob.glob("data/**/", recursive=False)
-configs = OmegaConf.create(
+sample_target_sound_dir_paths = list(glob.glob("data/**/", recursive=False))
+
+
+def test__init__():
+    configs = OmegaConf.create(
         {
             "dataset_dirs": sample_target_sound_dir_paths,
             "file_exts": [".wav"],
             "action_scaler": 1.0,
+            "low": -10.0,
+            "high": 10.0,
         }
     )
-
-def test__init__():
     env = make_env(**configs)
     assert isinstance(env, gym.Env)
+
+    assert isinstance(env.observation_space, Box)
+    assert isinstance(env.action_space, Box)
+
+
+def test_create_file_list():
+    # create temporary directories
+    temp_dir1 = tempfile.TemporaryDirectory()
+    temp_dir2 = tempfile.TemporaryDirectory()
+    
+    # create temporary files
+    temp_file1 = tempfile.NamedTemporaryFile(dir=temp_dir1.name, suffix='.wav')
+    temp_file2 = tempfile.NamedTemporaryFile(dir=temp_dir1.name, suffix='.flac')
+    temp_file3 = tempfile.NamedTemporaryFile(dir=temp_dir2.name, suffix='.txt')
+    
+    # test1: normal case
+    dataset_dirs = [temp_dir1.name, temp_dir2.name]
+    file_exts = [".wav", ".flac"]
+    expected_output = [temp_file1.name, temp_file2.name]
+    assert create_file_list(dataset_dirs, file_exts) == expected_output
+    
+    # test2: case of directory not exist
+    dataset_dirs = [temp_dir1.name, "not_exist_dir"]
+    file_exts = [".wav", ".flac"]
+    expected_output = [temp_file1.name, temp_file2.name]
+    assert create_file_list(dataset_dirs, file_exts) == expected_output # not_exist_dir is not a directory.
+
+    # test3: case of empty input
+    dataset_dirs = []
+    file_exts = [".wav", ".flac"]
+    expected_output = []
+    assert create_file_list(dataset_dirs, file_exts) == expected_output
+
+    # test4: case of empty output 
+    dataset_dirs = [temp_dir1.name]
+    file_exts = [".mp3"]
+    expected_output = []
+    assert create_file_list(dataset_dirs, file_exts) == expected_output
+    
+    # clean up temporary directories
+    temp_dir1.cleanup()
+    temp_dir2.cleanup()
+    
+    # test for sample_target_sound_dir_paths
+    assert create_file_list(sample_target_sound_dir_paths, [".wav"]) == glob.glob("data/**/*.wav", recursive=True)
+
+
+# test__init__()
+test_create_file_list()
