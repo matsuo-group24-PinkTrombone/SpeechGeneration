8cf9256faf21b58cbcd8f1cda1600f85a3d2a7e2
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-01-31 13:14:43 +0000

    FIX download dir and unzip downloaded

--- scripts/download_jsut.py
+++ scripts/download_jsut.py
@@ -1,9 +1,11 @@
-import subprocess
-import pathlib
 import os
+import pathlib
+import subprocess
+
 url = "http://ss-takashi.sakura.ne.jp/corpus/jsut_ver1.1.zip"
-output_dir = pathlib.Path(__file__).parents[1].joinpath("data/jsut")
+output_dir = pathlib.Path(__file__).parents[1].joinpath("data")
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-output_dir = output_dir.joinpath("jsut.zip")
-res = subprocess.call(f'curl "{url}" -o {output_dir}', shell=True)
+output_zip_dir = output_dir.joinpath("jsut.zip")
+res = subprocess.call(f'curl "{url}" -o "{output_zip_dir}"', shell=True)
+res = subprocess.call(f'unzip "{output_zip_dir}" -d "{output_dir}"', shell=True)
