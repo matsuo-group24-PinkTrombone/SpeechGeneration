d6b2f47e3d88d9c230dc7a8859491af86d1f54f5
cehl-kurage <yusakuishihira@gmail.com>
2023-02-01 11:08:49 +0900

    Fix make_random_dataset.py, download_jsut.py

--- scripts/download_jsut.py
+++ scripts/download_jsut.py
@@ -1,11 +1,10 @@
-import os
 import pathlib
 import subprocess
 
 url = "http://ss-takashi.sakura.ne.jp/corpus/jsut_ver1.1.zip"
 output_dir = pathlib.Path(__file__).parents[1].joinpath("data")
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+if not output_dir.exists():
+    output_dir.mkdir(parents=True)
 output_zip_dir = output_dir.joinpath("jsut.zip")
 res = subprocess.call(f'curl "{url}" -o "{output_zip_dir}"', shell=True)
 res = subprocess.call(f'unzip "{output_zip_dir}" -d "{output_dir}"', shell=True)
