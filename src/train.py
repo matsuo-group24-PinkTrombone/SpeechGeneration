3f66dce435c6c2a8308f1331e73ce25f30136723
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-02-05 14:11:14 +0000

    ADD train process

--- src/train.py
+++ src/train.py
@@ -1,10 +1,18 @@
 # このファイルは直接実行するため、相対import文を記述しないでください。
 import logging
+import random
 from typing import Optional
 
+import gym
 import hydra
+import numpy as np
 import pyrootutils
-from omegaconf import DictConfig
+import torch
+from omegaconf import DictConfig, OmegaConf
+
+from src.datamodules.replay_buffer import ReplayBuffer
+from src.models.dreamer import Dreamer
+from src.trainer import Trainer
 
 pyrootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
@@ -31,7 +39,40 @@ logger = logging.getLogger(__name__)
 
 def train(cfg: DictConfig) -> None:
     """Trains the model."""
-    logger.info("Training started!")
+
+    if cfg.get("seed") is not None:
+        seed = cfg.get("seed")
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        logger.info(f"Reset seed: {seed}")
+
+    logger.info(f"Instantiating env <{cfg.env._target_}>")
+    env: gym.Env = hydra.utils.instantiate(cfg.env)
+
+    logger.info(f"Instantiating model <{cfg.model._target_}>")
+    model: Dreamer = hydra.utils.instantiate(cfg.model)
+
+    logger.info(f"Instantiating replay_buffer <{cfg.replay_buffer._target_}>")
+    replay_buffer = hydra.utils.instantiate(cfg.replay_buffer)
+    replay_buffer: ReplayBuffer = replay_buffer(spaces=model.configure_replay_buffer_space(env))
+
+    logger.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer)
+
+    OmegaConf.resolve(cfg)
+    logger.info(f"Training configs:\n{OmegaConf.to_yaml(cfg)}")
+
+    logger.info("Training start!")
+
+    output = trainer.fit(env, replay_buffer, model)
+
+    del env
+
+    logger.info("Training finished.")
+
+    return output
 
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="train.yaml")
