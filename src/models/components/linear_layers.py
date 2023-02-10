5e9d96f41ed71316de530984b7427c4a95ffd2f8
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-02-10 13:26:26 +0000

    ADD normalization

--- src/models/components/linear_layers.py
+++ src/models/components/linear_layers.py
@@ -1,19 +1,14 @@
 import torch
+import torch.nn as nn
 
 
-class LinearLayers(torch.nn.Module):
+class LinearLayers(nn.Module):
+    """Linearの幅と深さを変更できるクラス."""
 
     def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        layers: int,
-        output_size: int,
-        bias: bool = True
-    ) -> torch.nn.Module:
+        self, input_size: int, hidden_size: int, layers: int, output_size: int, bias: bool = True
+    ) -> None:
         """
-        Linearの幅と深さを変更できるクラス
-
         Args:
             input_size (int):
             hidden_size (int):
@@ -22,15 +17,16 @@ class LinearLayers(torch.nn.Module):
             bias (bool):
         """
         super().__init__()
-        self.input_layer = torch.nn.Linear(input_size,hidden_size,bias=bias)
+        self.input_layer = nn.Linear(input_size, hidden_size, bias=bias)
 
-        self.hidden_layers = torch.nn.Sequential()
+        self.hidden_layers = nn.Sequential()
 
-        for i in range(layers):
-            self.hidden_layers.add_module(f"linear_{i}",torch.nn.Linear(hidden_size,hidden_size,bias=bias))
-            self.hidden_layers.add_module(f"ReLU_{i}",torch.nn.ReLU())
-        
-        self.output_layer = torch.nn.Linear(hidden_size,output_size,bias=bias)
+        for _ in range(layers):
+            self.hidden_layers.append(nn.Linear(hidden_size, hidden_size, bias=bias))
+            self.hidden_layers.append(nn.LayerNorm(hidden_size))
+            self.hidden_layers.append(nn.ReLU())
+
+        self.output_layer = nn.Linear(hidden_size, output_size, bias=bias)
 
     def forward(
         self,
@@ -45,7 +41,7 @@ class LinearLayers(torch.nn.Module):
         """
 
         hidden_first = self.input_layer(x)
-        
+
         hidden_last = self.hidden_layers(hidden_first)
 
         output = self.output_layer(hidden_last)
