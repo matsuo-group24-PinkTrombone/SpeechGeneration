654502c8277f1b34aa85df8d1302ade937a569c7
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-02-14 03:04:55 +0000

    Rewrite by chatgpt

--- tests/env/test_log_mel_spectrogram.py
+++ tests/env/test_log_mel_spectrogram.py
@@ -7,42 +7,39 @@ from pynktrombonegym.env import PynkTrombone
 from pynktrombonegym.spaces import ObservationSpaceNames as OSN
 from pynktrombonegym.spectrogram import calc_rfft_channel_num
 
-from src.env import log_mel_spectrogram as mod
+from src.env.log_mel_spectrogram import LogMelSpectrogram
 
-cls = mod.LogMelSpectrogram
-
-sample_target_sound_file_paths = glob.glob("data/sample_target_sounds/*")
+SAMPLE_TARGET_SOUND_FILE_PATHS = glob.glob("data/sample_target_sounds/*")
 
 
 def test__init__():
-    base_env = PynkTrombone(sample_target_sound_file_paths)
+    base_env = PynkTrombone(SAMPLE_TARGET_SOUND_FILE_PATHS)
     n_mels = 80
     log_offset = 1e-6
     n_fft = base_env.stft_window_size
     dtype = np.float32
     fft_c = calc_rfft_channel_num(n_fft)
     filter_bank_shape = (n_mels, fft_c)
-    wrapper = cls(
+    wrapper = LogMelSpectrogram(
         base_env, base_env.sample_rate, base_env.stft_window_size, n_mels, log_offset, dtype
     )
-
     assert wrapper.sample_rate == base_env.sample_rate
     assert wrapper.n_fft == n_fft
     assert wrapper.n_mels == n_mels
     assert wrapper.log_offset == log_offset
-    assert wrapper.mel_filter_banck.shape == filter_bank_shape
-    assert wrapper.mel_filter_banck.dtype == dtype
+    assert wrapper.mel_filter_bank.shape == filter_bank_shape
+    assert wrapper.mel_filter_bank.dtype == dtype
 
     del base_env
 
 
 def test_define_observation_space():
-    base_env = PynkTrombone(sample_target_sound_file_paths)
+    base_env = PynkTrombone(SAMPLE_TARGET_SOUND_FILE_PATHS)
     sample_rate = base_env.sample_rate
     n_fft = base_env.stft_window_size
     n_mels = 80
     spect_len = base_env.observation_space[OSN.TARGET_SOUND_SPECTROGRAM].shape[1]
-    observation_wrapper = cls(base_env, sample_rate, n_fft, n_mels)
+    observation_wrapper = LogMelSpectrogram(base_env, sample_rate, n_fft, n_mels)
 
     obs_space = observation_wrapper.define_observation_space()
 
@@ -56,11 +53,11 @@ def test_define_observation_space():
 
 
 def test_observation():
-    env = PynkTrombone(sample_target_sound_file_paths)
+    env = PynkTrombone(SAMPLE_TARGET_SOUND_FILE_PATHS)
     sample_rate = env.sample_rate
     n_fft = env.stft_window_size
     n_mels = 80
-    log_mel_spectrogram = cls(env, sample_rate, n_fft, n_mels)
+    log_mel_spectrogram = LogMelSpectrogram(env, sample_rate, n_fft, n_mels)
     shape = log_mel_spectrogram.observation_space[OSN.TARGET_SOUND_SPECTROGRAM].shape
 
     obs = env.reset()
@@ -76,16 +73,16 @@ def test_observation():
 
 
 def test_log_mel():
-    env = PynkTrombone(sample_target_sound_file_paths)
+    env = PynkTrombone(SAMPLE_TARGET_SOUND_FILE_PATHS)
     sample_rate = env.sample_rate
     n_mels = 80
     n_fft = env.stft_window_size
-    log_mel_spectrogram = cls(env, sample_rate, n_fft, n_mels)
-    shape = env.observation_space[OSN.TARGET_SOUND_SPECTROGRAM].shape
-    logmel_shape = (n_mels, shape[1])
-    zero_spect = np.zeros(shape)
+    log_mel_spectrogram = LogMelSpectrogram(env, sample_rate, n_fft, n_mels)
+    target_spectrogram_shape = env.observation_space[OSN.TARGET_SOUND_SPECTROGRAM].shape
+    logmel_shape = (n_mels, target_spectrogram_shape[1])
+    zero_spectrogram = np.zeros(target_spectrogram_shape)
 
-    zero_logmel = log_mel_spectrogram.log_mel(zero_spect)
+    zero_logmel = log_mel_spectrogram.log_mel(zero_spectrogram)
 
     assert isinstance(zero_logmel, np.ndarray)
     assert zero_logmel.shape == logmel_shape
