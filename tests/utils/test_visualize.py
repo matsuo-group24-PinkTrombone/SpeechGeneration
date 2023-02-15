48632307f13f826f987a6b243ae0d15e33c9b365
cehl-kurage <yusakuishihira@gmail.com>
2023-02-15 21:28:32 +0900

    ADD tests for visualize

--- tests/utils/test_visualize.py
+++ tests/utils/test_visualize.py
@@ -1,30 +1,26 @@
-from src.utils.visualize import make_spectrogram_figure, visualize_model_approximation
 import glob
 import os
 import pathlib
 from datetime import datetime
 from functools import partial
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pytest
 import torch
 from gym.spaces import Box
 from pynktrombonegym.spaces import ObservationSpaceNames as OSN
-
-from src.env.make_env import make_env
 from torch.optim import SGD
 from torch.utils.tensorboard import SummaryWriter
 
 from src.datamodules import buffer_names
 from src.datamodules.replay_buffer import ReplayBuffer
 from src.env.array_action import ARRAY_ORDER as AO_act
-
 from src.env.array_voc_state import ARRAY_ORDER as AO_voc
 from src.env.array_voc_state import VSON
-from src.utils.visualize import make_spectrogram_figure
-
+from src.env.make_env import make_env
 from src.models.dreamer import Dreamer
+from src.utils.visualize import make_spectrogram_figure, visualize_model_approximation
 from tests.models.abc.dummy_classes import DummyAgent as DA
 from tests.models.abc.dummy_classes import DummyController as DC
 from tests.models.abc.dummy_classes import DummyObservationDecoder as DOD
@@ -77,7 +73,7 @@ bf_space = {
 args = (trans, prior, obs_enc, obs_dec, ctrl, d_world, d_agent, world_opt, ctrl_opt)
 del env
 
-tb_log_dir = f"logs/test_dreamer/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+tb_log_dir = f"logs/test_visualize/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
 
 
 def test_make_spectrogram_figure():
@@ -90,13 +86,13 @@ def test_make_spectrogram_figure():
     tag = "evaluation_step/mel_spect/test"
     for i in range(3):
         fig: plt.figure = make_spectrogram_figure(tgt_spect, gen_spect, pred_gen_spect)
-        model.tensorboard.add_figure(tag, fig, global_step=i+1)
-    
+        model.tensorboard.add_figure(tag, fig, global_step=i + 1)
+
 
 def test_visualize_model_approximation():
     env = make_env([target_file_path])
     model = Dreamer(*args)
-    tb=SummaryWriter(os.path.join(tb_log_dir, "test_log"))
+    tb = SummaryWriter(os.path.join(tb_log_dir, "test_log"))
     world = model.world
     agent = model.agent
 
